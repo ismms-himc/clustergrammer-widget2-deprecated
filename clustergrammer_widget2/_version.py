ad75444f2af0617b2bd0a0f496c44ad466739fa1
cornhundred <nickfloresfernandez@gmail.com>
2018-08-28 14:40:15 -0400

    going to publish 0.4.0 to override older versions

--- clustergrammer_widget2/_version.py
+++ clustergrammer_widget2/_version.py
@@ -4,7 +4,7 @@
 # Copyright (c) Nicolas Fernandez.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 1, 0, 'dev')
+version_info = (0, 4, 0, 'dev')
 __version__ = ".".join(map(str, version_info))
 
 # The version of the attribute spec that this package
