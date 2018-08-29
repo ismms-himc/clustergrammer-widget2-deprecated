beba164869fae10e80fafe6d6dd222f95496f271
cornhundred <nickfloresfernandez@gmail.com>
2018-08-29 16:13:30 -0400

    saved widget with 0.4.4 version

--- src/widget.ts
+++ src/widget.ts
@@ -13,7 +13,7 @@ import cgm_fun from 'clustergrammer-gl';
 
 import * as d3 from 'd3';
 
-console.log('version 0.4.3, changed embed entry ')
+console.log('version 0.4.4, changed embed entry ')
 console.log(cgm_fun);
 console.log(d3)
 
